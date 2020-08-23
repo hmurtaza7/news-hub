d06a1116a9e47be69b773fe551a454abc20eb85e
Hassan <hassan.a.murtaza@gmail.com>
2020-08-24 00:44:48 +0500

    added like animation

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,14 @@
 module PostsHelper
 
   def like_path post_id
-    link_to 'Like', post_likes_path(post_id), remote: true, method: :post
+    link_to post_likes_path(post_id), remote: true, method: :post do
+      "<i class=\"fas fa-heart\"></i>".html_safe
+    end
   end
 
   def unlike_path post_id
-    link_to 'Unlike', post_likes_path(post_id), remote: true, method: :delete
+    link_to post_likes_path(post_id), remote: true, method: :delete do
+      "<i class=\"fas fa-heart-broken\"></i>".html_safe
+    end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+// $(document).ready(() => {
+//   $('.posts').on('click', '.fa-heart, .fa-heart-broken', (e) => {
+//     $(e.target).off('click');
+//   });
+// });
